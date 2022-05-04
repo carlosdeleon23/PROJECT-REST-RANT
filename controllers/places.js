1fca89045049a136875e8c608b5a31d0bba53191
Carlos De Leon <carlosdeleon@Carloss-MacBook-Pro.local>
2022-05-03 22:41:37 -0400

    Adding places view with mock data

--- controllers/places.js
+++ controllers/places.js
@@ -1,44 +1,25 @@
 const router = require('express').Router()
 
-//ALL GETs
+// GET /places
 router.get('/', (req, res) => {
-    res.send('GET/places/index')
-});
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: 'http://placekitten.com/250/250'
+      }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: 'http://placekitten.com/250/250'
+      }]
+      
+      res.render('places/index', { places })
 
-router.get('/new', (req, res) => {
-    res.send('GET NEW/places/new')
-});
-
-router.get('/:id', (req, res) => {
-    res.send('GET ID/places/${req.paramas.id}')
-});
-
-router.get('/:id/edit', (req, res) => {
-    res.send('GET EDIT/places/${req.paramas.id}/edit')
-});
-
-router.get('*', (req, res) => {
-    res.send('GET 404/*')
-});
-
-//ALL POSTs
-router.post('/', (req, res) => {
-    res.send('POST/places/index')
-});
-
-router.post('/:id/rant', (req, res) => {
-    res.send('POST/places/index/${req.params.id}/rant')
-});
-
-//PUT
-router.put('/:id/', (req, res) => {
-    res.send('PUT/index/${req.params.id}')
-});
-
-//ALL DELETEs
-router.delete('/:id/rant/:rantId', (req, res) => {
-    res.send('Delete/index/${req.params.id}/rant${req.params.rantId')
-});
+  })
+  
 
 
 module.exports = router
